b62be88611318f24a8b8164ac9d53b856cd37bab
SarveshSW <sarvesh.wanzare@icloud.com>
2024-05-14 17:31:44 +0400

    finished with the typing effect.

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+const nameElement = document.getElementById('typing-name');
+const cursorElement = document.getElementById('cursor-blnk');
+const name = "Sarvesh Wanzare";
+let index = 0;
+
+function type() {
+    if (index < name.length) {
+        nameElement.innerHTML += name.charAt(index);
+        index++;
+        setTimeout(type, 100); // Adjust typing speed here (in milliseconds)
+    } else {
+        cursorElement.style.display = 'inline-block'; // Show cursor after typing finishes
+    }
+}
+
+type();
